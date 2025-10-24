3985ff9166f0c480de1b43b4f0b1901f471b7579
Yusuf Darwish <105949780+Yerbis@users.noreply.github.com>
2025-10-23 18:08:06 -0700

    Update author information in masjid_display.py

--- code/masjid_display.py
+++ code/masjid_display.py
@@ -1,7 +1,7 @@
 """
 Started Oct 20 2025 @14:40
 
-Created by Yusuf Darwish, designed by Mahdi Elghazali and Omar Nassar
+Updated by Yusuf Darwish, designed by Mahdi Elghazali and Omar Nassar
 
 @author: Mahdi Elghazali and Omar Nassar
 
